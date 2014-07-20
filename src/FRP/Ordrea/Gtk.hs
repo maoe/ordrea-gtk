aef2470a8f865f7c3a35d35bc75f4df3720014fa
Mitsutoshi Aoe <maoe@foldr.in>
2014-07-20 22:09:27 +0900

    Add Gtk monad

--- src/FRP/Ordrea/Gtk.hs
+++ src/FRP/Ordrea/Gtk.hs
@@ -1,59 +1,101 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module FRP.Ordrea.Gtk
-  ( liftGtk
+  ( Gtk
+  , runGtk, runGtkWith
+  , defaultGtkMain
+  , liftSGen
+
+  , liftGtk
   , liftGtkAsync
 
   , performGtk
   , performGtkAsync
 
-  , runGUI
-  , defaultMain
-
   , on
+  , after
   ) where
+
 import Control.Applicative
 import Control.Concurrent
 import Control.Monad
 import Control.Monad.Trans
+import Control.Monad.Writer
 
 import FRP.Ordrea
 import FRP.Ordrea.IO
-import Graphics.UI.Gtk hiding (Signal, on)
+import Graphics.UI.Gtk hiding (after, on)
 import qualified Graphics.UI.Gtk as Gtk
 
-performGtk :: Event (IO a) -> SignalGen (Event a)
-performGtk = perform postGUISync
+newtype Gtk a = Gtk { unGtk :: WriterT (Event ()) SignalGen a }
+  deriving
+    ( Functor, Applicative, Monad, MonadFix
+    , MonadIO, MonadWriter (Event ())
+    )
 
-performGtkAsync :: Event (IO ()) -> SignalGen (Event ())
-performGtkAsync = perform postGUIAsync
+runGtk :: Gtk a -> IO ()
+runGtk = runGtkWith 10
+
+runGtkWith :: Int -> Gtk a -> IO ()
+runGtkWith timeout (Gtk w) = do
+  action <- start $ do
+    (_, event) <- runWriterT w
+    return $ eventToBehavior event
+  void $ timeoutAdd (True <$ action) timeout
+
+liftSGen :: SignalGen a -> Gtk a
+liftSGen = Gtk . lift
+
+defaultGtkMain :: Gtk a -> IO ()
+defaultGtkMain gtk = do
+  initGUI
+  void . forkIO $ runGtk gtk
+  mainGUI
+
+performGtk :: Event (IO a) -> Gtk ()
+performGtk = liftSGen . perform postGUISync >=> registerGtk
+
+performGtkAsync :: Event (IO a) -> Gtk ()
+performGtkAsync =
+  liftSGen . perform (postGUIAsync . void) >=> registerGtk
+
+registerGtk :: Event a -> Gtk ()
+registerGtk event = tell $ () <$ event
 
 liftGtk :: MonadIO m => IO a -> m a
 liftGtk = liftIO . postGUISync
 
-liftGtkAsync :: MonadIO m => IO () -> m ()
-liftGtkAsync = liftIO . postGUIAsync
+liftGtkAsync :: MonadIO m => IO a -> m ()
+liftGtkAsync = liftIO . postGUIAsync . void
 
-runGUI :: SignalGen (Event ()) -> IO ()
-runGUI gtkGen = do
-  gtkAction <- start $ eventToSignal <$> gtkGen
-  void $ timeoutAdd (True <$ gtkAction) 10
-
-defaultMain :: SignalGen (Event ()) -> IO ()
-defaultMain app = do
-  initGUI
-  void . forkIO $ runGUI app
-  mainGUI
-
+-- | Perform an action in response to a GTK signal.
 on
   :: MonadIO m
   => object
-  -> Gtk.Signal object (m a)
+  -> Signal object (m a)
   -> m a
-  -> SignalGen (ConnectId object, Event a)
-on object signal callback = do
-  extE <- liftIO newExternalEvent
-  connId <- liftGtk $ Gtk.on object signal $ do
+  -> Gtk (ConnectId object, Event a)
+on object signal = connectSignal (Gtk.on object signal)
+
+-- | Perform an action in response to a GTK signal like @on@, but the signal
+-- is executed after GTK's default handler has run.
+after
+  :: MonadIO m
+  => object
+  -> Signal object (m a)
+  -> m a
+  -> Gtk (ConnectId object, Event a)
+after object signal = connectSignal (Gtk.after object signal)
+
+connectSignal
+  :: MonadIO m
+  => (m a -> IO (ConnectId object))
+  -> m a
+  -> Gtk (ConnectId object, Event a)
+connectSignal f callback = do
+  extern <- liftIO newExternalEvent
+  connId <- liftGtk $ f $ do
       a <- callback
-      liftIO $ triggerExternalEvent extE a
+      liftIO $ triggerExternalEvent extern a
       return a
-  event <- externalE extE
+  event <- liftSGen $ externalE extern
   return (connId, event)
